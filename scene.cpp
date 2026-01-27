8e8c7e0e485926e91ac653993170a53f3be8b381
Jacco Bikker <bikker.j@protonmail.com>
2026-01-27 11:50:44 +0100

    Removed distinction between GRID and WORLD.

--- scene.cpp
+++ scene.cpp
@@ -23,8 +23,8 @@ inline bool point_in_cube( const float3& pos )
 Scene::Scene()
 {
 	// allocate room for the world
-	grid = (uint*)MALLOC64( GRIDSIZE3 * sizeof( uint ) );
-	memset( grid, 0, GRIDSIZE3 * sizeof( uint ) );
+	grid = (uint*)MALLOC64( WORLDSIZE3 * sizeof( uint ) );
+	memset( grid, 0, WORLDSIZE3 * sizeof( uint ) );
 	// initialize the scene using Perlin noise, parallel over z
 #pragma omp parallel for schedule(dynamic)
 	for (int z = 0; z < 128; z++)
@@ -44,7 +44,7 @@ Scene::Scene()
 
 void Scene::Set( const uint x, const uint y, const uint z, const uint v )
 {
-	grid[x + y * GRIDSIZE + z * GRIDSIZE2] = v;
+	grid[x + y * WORLDSIZE + z * WORLDSIZE2] = v;
 }
 
 bool Scene::Setup3DDDA( Ray& ray, DDAState& state ) const
@@ -58,16 +58,16 @@ bool Scene::Setup3DDDA( Ray& ray, DDAState& state ) const
 		if (state.t > 1e33f) return false; // ray misses voxel data entirely
 	}
 	// setup amanatides & woo - assume world is 1x1x1, from (0,0,0) to (1,1,1)
-	static const float cellSize = 1.0f / GRIDSIZE;
+	static const float cellSize = 1.0f / WORLDSIZE;
 	state.step = make_int3( 1.0f - ray.Dsign * 2.0f );
-	const float3 posInGrid = (float)GRIDSIZE * (ray.O + (state.t + 0.00005f) * ray.D);
+	const float3 posInGrid = (float)WORLDSIZE * (ray.O + (state.t + 0.00005f) * ray.D);
 	const float3 gridPlanes = (ceilf( posInGrid ) - ray.Dsign) * cellSize;
-	const int3 P = clamp( make_int3( posInGrid ), 0, GRIDSIZE - 1 );
+	const int3 P = clamp( make_int3( posInGrid ), 0, WORLDSIZE - 1 );
 	state.X = P.x, state.Y = P.y, state.Z = P.z;
 	state.tdelta = cellSize * float3( state.step ) * ray.rD;
 	state.tmax = (gridPlanes - ray.O) * ray.rD;
 	// detect rays that start inside a voxel
-	uint cell = grid[P.x + P.y * GRIDSIZE + P.z * GRIDSIZE2];
+	uint cell = grid[P.x + P.y * WORLDSIZE + P.z * WORLDSIZE2];
 	ray.inside = cell != 0 && startedInGrid;
 	// proceed with traversal
 	return true;
@@ -86,18 +86,18 @@ void Scene::FindNearest( Ray& ray ) const
 		// start stepping until we find an empty voxel
 		while (1)
 		{
-			cell = grid[s.X + s.Y * GRIDSIZE + s.Z * GRIDSIZE2];
+			cell = grid[s.X + s.Y * WORLDSIZE + s.Z * WORLDSIZE2];
 			if (!cell) break;
 			lastCell = cell;
 			if (s.tmax.x < s.tmax.y)
 			{
-				if (s.tmax.x < s.tmax.z) { s.t = s.tmax.x, s.X += s.step.x, axis = 0; if (s.X >= GRIDSIZE) break; s.tmax.x += s.tdelta.x; }
-				else { s.t = s.tmax.z, s.Z += s.step.z, axis = 2; if (s.Z >= GRIDSIZE) break; s.tmax.z += s.tdelta.z; }
+				if (s.tmax.x < s.tmax.z) { s.t = s.tmax.x, s.X += s.step.x, axis = 0; if (s.X >= WORLDSIZE) break; s.tmax.x += s.tdelta.x; }
+				else { s.t = s.tmax.z, s.Z += s.step.z, axis = 2; if (s.Z >= WORLDSIZE) break; s.tmax.z += s.tdelta.z; }
 			}
 			else
 			{
-				if (s.tmax.y < s.tmax.z) { s.t = s.tmax.y, s.Y += s.step.y, axis = 1; if (s.Y >= GRIDSIZE) break; s.tmax.y += s.tdelta.y; }
-				else { s.t = s.tmax.z, s.Z += s.step.z, axis = 2; if (s.Z >= GRIDSIZE) break; s.tmax.z += s.tdelta.z; }
+				if (s.tmax.y < s.tmax.z) { s.t = s.tmax.y, s.Y += s.step.y, axis = 1; if (s.Y >= WORLDSIZE) break; s.tmax.y += s.tdelta.y; }
+				else { s.t = s.tmax.z, s.Z += s.step.z, axis = 2; if (s.Z >= WORLDSIZE) break; s.tmax.z += s.tdelta.z; }
 			}
 		}
 		ray.voxel = lastCell; // we store the voxel we just left
@@ -107,16 +107,16 @@ void Scene::FindNearest( Ray& ray ) const
 		// start stepping until we find a filled voxel
 		while (1)
 		{
-			cell = grid[s.X + s.Y * GRIDSIZE + s.Z * GRIDSIZE2];
+			cell = grid[s.X + s.Y * WORLDSIZE + s.Z * WORLDSIZE2];
 			if (cell) break; else if (s.tmax.x < s.tmax.y)
 			{
-				if (s.tmax.x < s.tmax.z) { s.t = s.tmax.x, s.X += s.step.x, axis = 0; if (s.X >= GRIDSIZE) break; s.tmax.x += s.tdelta.x; }
-				else { s.t = s.tmax.z, s.Z += s.step.z, axis = 2; if (s.Z >= GRIDSIZE) break; s.tmax.z += s.tdelta.z; }
+				if (s.tmax.x < s.tmax.z) { s.t = s.tmax.x, s.X += s.step.x, axis = 0; if (s.X >= WORLDSIZE) break; s.tmax.x += s.tdelta.x; }
+				else { s.t = s.tmax.z, s.Z += s.step.z, axis = 2; if (s.Z >= WORLDSIZE) break; s.tmax.z += s.tdelta.z; }
 			}
 			else
 			{
-				if (s.tmax.y < s.tmax.z) { s.t = s.tmax.y, s.Y += s.step.y, axis = 1; if (s.Y >= GRIDSIZE) break; s.tmax.y += s.tdelta.y; }
-				else { s.t = s.tmax.z, s.Z += s.step.z, axis = 2; if (s.Z >= GRIDSIZE) break; s.tmax.z += s.tdelta.z; }
+				if (s.tmax.y < s.tmax.z) { s.t = s.tmax.y, s.Y += s.step.y, axis = 1; if (s.Y >= WORLDSIZE) break; s.tmax.y += s.tdelta.y; }
+				else { s.t = s.tmax.z, s.Z += s.step.z, axis = 2; if (s.Z >= WORLDSIZE) break; s.tmax.z += s.tdelta.z; }
 			}
 		}
 		ray.voxel = cell;
@@ -136,17 +136,17 @@ bool Scene::IsOccluded( Ray& ray ) const
 	// start stepping
 	while (s.t < ray.t)
 	{
-		const uint cell = grid[s.X + s.Y * GRIDSIZE + s.Z * GRIDSIZE2];
+		const uint cell = grid[s.X + s.Y * WORLDSIZE + s.Z * WORLDSIZE2];
 		if (cell) /* we hit a solid voxel */ return s.t < ray.t;
 		if (s.tmax.x < s.tmax.y)
 		{
-			if (s.tmax.x < s.tmax.z) { if ((s.X += s.step.x) >= GRIDSIZE) return false; s.t = s.tmax.x, s.tmax.x += s.tdelta.x; }
-			else { if ((s.Z += s.step.z) >= GRIDSIZE) return false; s.t = s.tmax.z, s.tmax.z += s.tdelta.z; }
+			if (s.tmax.x < s.tmax.z) { if ((s.X += s.step.x) >= WORLDSIZE) return false; s.t = s.tmax.x, s.tmax.x += s.tdelta.x; }
+			else { if ((s.Z += s.step.z) >= WORLDSIZE) return false; s.t = s.tmax.z, s.tmax.z += s.tdelta.z; }
 		}
 		else
 		{
-			if (s.tmax.y < s.tmax.z) { if ((s.Y += s.step.y) >= GRIDSIZE) return false; s.t = s.tmax.y, s.tmax.y += s.tdelta.y; }
-			else { if ((s.Z += s.step.z) >= GRIDSIZE) return false; s.t = s.tmax.z, s.tmax.z += s.tdelta.z; }
+			if (s.tmax.y < s.tmax.z) { if ((s.Y += s.step.y) >= WORLDSIZE) return false; s.t = s.tmax.y, s.tmax.y += s.tdelta.y; }
+			else { if ((s.Z += s.step.z) >= WORLDSIZE) return false; s.t = s.tmax.z, s.tmax.z += s.tdelta.z; }
 		}
 	}
 	return false;
